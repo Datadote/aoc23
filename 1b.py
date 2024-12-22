d7dfb9c3119aa716628d614c05259b33055fef91
Datadote <datadote@gmail.com>
2024-12-22 11:14:51 -0800

    refactored 1b.py to add dictionary of str(int): int, and also use s[i:].startswith(k)

--- 1b.py
+++ 1b.py
@@ -1,20 +1,17 @@
 from aocd import get_data
 inp = get_data(day=1, year=2023)
 
+nums = {str(i): i for i in range(10)}
 number_words = {'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
                 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9, 'zero': 0
 }
+number_words = {**nums, **number_words}
 
 def get_num(s):
     first, last = None, None
     for i in range(len(s)):
-        c = s[i]
-        if c.isdigit():
-            if first is None:
-                first = int(c)
-            last = int(c)
         for k, v in number_words.items():
-            if s[i:i + len(k)] == k:
+            if s[i:].startswith(k):
                 if first is None:
                     first = number_words[k]
                 last = number_words[k]
